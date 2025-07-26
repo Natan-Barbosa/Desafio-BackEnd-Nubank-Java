2217ba335d2a687ce2dab55b9dab3d1ff7bf3d5d
natan <natanbarbosax193@gmail.com>
2025-07-26 17:57:59 -0300

    add: contacts service

---
+++ src/main/java/com/barbosa/desafio_backend_nubank_JAVA/service/Contacts/ContactsService.java
@@ -0,0 +1,35 @@
+package com.barbosa.desafio_backend_nubank_JAVA.service.Contacts;
+
+import com.barbosa.desafio_backend_nubank_JAVA.entities.ClientEntity;
+import com.barbosa.desafio_backend_nubank_JAVA.entities.ContactEntity;
+import com.barbosa.desafio_backend_nubank_JAVA.exceptions.ClientNotFoundException;
+import com.barbosa.desafio_backend_nubank_JAVA.repository.ClientRepository;
+import com.barbosa.desafio_backend_nubank_JAVA.repository.ContactsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactsService {
+
+    @Autowired
+    private ContactsRepository contactsRepository;
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public ContactEntity create(ContactsCreateDto createDto) {
+        ClientEntity client = clientRepository.findById(createDto.getClientID())
+                .orElseThrow(() -> new ClientNotFoundException("The Sended Client Not Exists"));
+
+        ContactEntity newContact = ContactEntity.builder()
+                .contactName(createDto.getContactName())
+                .contactNumber(createDto.getContactNumber())
+                .createdAt(createDto.getCreatedAt())
+                .client(client)
+                .build();
+
+        ContactEntity savedContact = contactsRepository.save(newContact);
+        return savedContact;
+    }
+
+}
