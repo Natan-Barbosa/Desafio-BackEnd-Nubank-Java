21b8c4aaf80658ff57da428404dd9e9fe6ed264f
natan <natanbarbosax193@gmail.com>
2025-07-26 19:46:36 -0300

    edit: add errors field to return list of errors

--- src/main/java/com/barbosa/desafio_backend_nubank_JAVA/utils/ExceptionModel.java
+++ src/main/java/com/barbosa/desafio_backend_nubank_JAVA/utils/ExceptionModel.java
@@ -1,17 +1,21 @@
 package com.barbosa.desafio_backend_nubank_JAVA.utils;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Builder
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ExceptionModel {
     private String title;
     private String description;
+    private Map<String, String> errors;
     private Integer code;
     private LocalDateTime timeStamp;
 }
