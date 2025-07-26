c06735d5abd325a4477f8d49f1fdd9a65c399a8c
natan <natanbarbosax193@gmail.com>
2025-07-26 14:33:13 -0300

    edit: fix params of type order

--- src/main/java/com/barbosa/desafio_backend_nubank_JAVA/repository/ContactsRepository.java
+++ src/main/java/com/barbosa/desafio_backend_nubank_JAVA/repository/ContactsRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContactsRepository extends JpaRepository<Long, ContactEntity> {
+public interface ContactsRepository extends JpaRepository<ContactEntity,Long> {
 }
