e87efdcd3afdf7aef203b86ac174ab14f49181f1
natan <natanbarbosax193@gmail.com>
2025-07-26 17:39:38 -0300

    add: Create Client Dto

---
+++ src/main/java/com/barbosa/desafio_backend_nubank_JAVA/service/Client/CreateClientDto.java
@@ -0,0 +1,23 @@
+package com.barbosa.desafio_backend_nubank_JAVA.service.Client;
+
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateClientDto {
+    @NotEmpty
+    private String cnpj;
+
+    @NotEmpty
+    private String name;
+
+    private final LocalDateTime createdAt = LocalDateTime.now();
+
+}
