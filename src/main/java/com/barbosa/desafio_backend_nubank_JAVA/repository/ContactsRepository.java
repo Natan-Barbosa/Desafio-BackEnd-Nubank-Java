fd915971bd83569df2ea51cbabe76f67b3918647
natan <natanbarbosax193@gmail.com>
2025-07-26 14:19:24 -0300

    add: Contacts Repository

---
+++ src/main/java/com/barbosa/desafio_backend_nubank_JAVA/repository/ContactsRepository.java
@@ -0,0 +1,9 @@
+package com.barbosa.desafio_backend_nubank_JAVA.repository;
+
+import com.barbosa.desafio_backend_nubank_JAVA.entities.ContactEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactsRepository extends JpaRepository<Long, ContactEntity> {
+}
