d1b14ddd909d4f5d56af839ddb0cafef00bf1a68
natan <natanbarbosax193@gmail.com>
2025-07-26 19:19:26 -0300

    edit: add constraints to validate the sended data

--- src/main/java/com/barbosa/desafio_backend_nubank_JAVA/service/Contacts/ContactsCreateDto.java
+++ src/main/java/com/barbosa/desafio_backend_nubank_JAVA/service/Contacts/ContactsCreateDto.java
@@ -1,5 +1,7 @@
 package com.barbosa.desafio_backend_nubank_JAVA.service.Contacts;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,10 +10,14 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class ContactsCreateDto {
+
+    @NotEmpty
     private String contactName;
 
+    @NotEmpty
     private String contactNumber;
 
+    @NotNull
     private Long clientID;
 
     private final LocalDateTime createdAt = LocalDateTime.now();
