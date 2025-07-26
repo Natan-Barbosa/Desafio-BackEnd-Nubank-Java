5e0589ce6e73424ef06d987d53bdf73a47d5b46a
natan <natanbarbosax193@gmail.com>
2025-07-26 11:28:27 -0300

    initial commit

---
+++ src/main/java/com/barbosa/desafio_backend_nubank_JAVA/DesafioBackendNubankJavaApplication.java
@@ -0,0 +1,13 @@
+package com.barbosa.desafio_backend_nubank_JAVA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendNubankJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendNubankJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbosa/desafio_backend_nubank_JAVA/DesafioBackendNubankJavaApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.barbosa.desafio_backend_nubank_JAVA;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DesafioBackendNubankJavaApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
