63ad120ca50414983dbe2ae07171f8176817d1f8
natan <natanbarbosax193@gmail.com>
2025-07-26 19:20:21 -0300

    edit: add get methods and validation in case client exists

--- src/main/java/com/barbosa/desafio_backend_nubank_JAVA/service/Client/ClientService.java
+++ src/main/java/com/barbosa/desafio_backend_nubank_JAVA/service/Client/ClientService.java
@@ -1,9 +1,13 @@
 package com.barbosa.desafio_backend_nubank_JAVA.service.Client;
 
 import com.barbosa.desafio_backend_nubank_JAVA.entities.ClientEntity;
+import com.barbosa.desafio_backend_nubank_JAVA.exceptions.ClientAlreadyExistsException;
+import com.barbosa.desafio_backend_nubank_JAVA.exceptions.ClientNotFoundException;
 import com.barbosa.desafio_backend_nubank_JAVA.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClientService {
@@ -12,6 +16,11 @@ public class ClientService {
     private ClientRepository clientRepository;
 
     public ClientEntity create(CreateClientDto clientDto) {
+        boolean clientExists = clientRepository.existsBycnpj(clientDto.getCnpj());
+        if (clientExists) {
+            throw new ClientAlreadyExistsException("Client With This CNPJ Already Exists!");
+        }
+
         ClientEntity client = ClientEntity.builder()
                 .name(clientDto.getName())
                 .cnpj(clientDto.getCnpj())
@@ -19,5 +28,16 @@ public class ClientService {
                 .build();
         ClientEntity savedClient = clientRepository.save(client);
         return savedClient;
+    }
+
+    public List<ClientEntity> get() {
+        List<ClientEntity> allClients = clientRepository.findAll();
+        return allClients;
+    }
+
+    public ClientEntity getClientById(Long id) {
+        ClientEntity client = clientRepository.findById(id)
+                .orElseThrow(() -> new ClientNotFoundException("The Sended Client Not Exists"));
+        return client;
     }
 }
